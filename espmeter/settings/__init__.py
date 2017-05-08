266d2e4e233a0be4d7ade48c3cdf4611862068a3
melfelr <melfelrac@gmail.com>
2017-05-08 19:44:29 +0300

    F

--- espmeter/settings/__init__.py
+++ espmeter/settings/__init__.py
@@ -1,8 +1,7 @@
-from espmeter.settings.local_settings import *
 try:
     from local_settings import *
 except ImportError:
     try:
         from production import *
     except ImportError:
-        from espmeter.settings.base import *
+        from base import *
